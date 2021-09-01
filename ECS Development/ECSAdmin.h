8ad27ed03a0201613dc27aa50568a151495c93a9
Wille2130 <32479398+Wille2130@users.noreply.github.com>
2021-09-01 23:53:12 +0200

    ECS

--- ECS Development/ECSAdmin.h
+++ ECS Development/ECSAdmin.h
@@ -19,33 +19,7 @@ public:
 	ECSAdmin() = default;
 	~ECSAdmin() = default;
 
-	void Update();
-
-	Entity CreateEntity();
-
-	template<typename T>
-	void AddComponent(const Entity& anEntity, Component* aComponent);
-
-	//template<typename T>
-	//T* GetComponent(const Entity& anEntity);
-
 private:
-	std::vector<Entity> myActiveEntities;
-	std::map<std::type_index, CU::ObjectPool<Component*>> myComponents;
 
-	Entity myNextEntityID = 0;
+
 };
-
-template<typename T>
-inline void ECSAdmin::AddComponent(const Entity& anEntity, Component* aComponent)
-{
-	std::type_index typeID = typeid(T);
-
-	if (!myComponents[typeID].IsInitialized())
-	{
-		CU::ObjectPool<Component*> pool = CU::ObjectPool<Component*>(MaxComponentCount);
-		myComponents[typeID] = pool;
-	}
-
-	myComponents[typeID].GetNextFree()->Value() = aComponent;
-}
